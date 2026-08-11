789237083525b448755a70f832bfe4f7ead01776
hungg2951 <hungtv2951@example.com>
2026-08-11 11:41:53 +0700

    feat: implement Shopify customer and segment services with GraphQL integration

--- src/shopify/customer/customer.service.ts
+++ src/shopify/customer/customer.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { ShopifyAuthService } from './shopify-auth.service';
+import { ShopifyAuthService } from '../shopify-auth.service';
 
 @Injectable()
 export class CustomerService {
--- src/shopify/shopify.module.ts
+++ src/shopify/shopify.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ShopifyAuthService } from './shopify-auth.service';
-import { CustomerService } from './customer.service';
-import { CustomerController } from './customer.controller';
-import { SegmentService } from './segment.service';
-import { SegmentController } from './segment.controller';
+import { CustomerService } from './customer/customer.service';
+import { CustomerController } from './customer/customer.controller';
+import { SegmentService } from './segment/segment.service';
+import { SegmentController } from './segment/segment.controller';
 
 @Module({
   controllers: [CustomerController, SegmentController],
@@ -11,4 +11,5 @@ import { SegmentController } from './segment.controller';
   exports: [ShopifyAuthService, CustomerService, SegmentService],
 })
 export class ShopifyModule {}
+
 
